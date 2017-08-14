44a4d00f807dc823c2ac37a75c83e1536efd689a
lglayat <glayatle@g.cofc.edu>
2017-08-14 15:59:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :songs, except: [:destroy]
+  resources :artists, except: [:destroy]
+  resources :genres, except: [:destroy]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
